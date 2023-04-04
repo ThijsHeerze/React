2e74018769749326a0ef7b9971288c2495209508
Thijs Heerze <thijsheerze@gmail.com>
2023-04-04 12:11:20 +0200

    pokemon api

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,90 @@
-import React, {useState} from "react";
-import "./App.css";
+import './App.css';
+import { useEffect, useState } from 'react';
+import Pokemon from './Pokemon';
 
+function App({}) {
+  var [counter, setCounter] = useState(1);
+  var [pokemons, setPokemons] = useState([]);
+  var [loading, setLoading] = useState(true);
+  var [favoPokemons, setFavoPokemons] = useState([]);
 
-//function Todo for remove and add todo's
-function Todo({todo, index, completeTodo, removeTodo}){
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then((response) => response.json())
+      .then((data) => {
+        setPokemons(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="pokemon">
+        Loading...
+      </div>
+    )
+  }
+
+  const getNextResults = () => {
+    setLoading(true);
+    fetch(pokemons.next)
+      .then((response) => response.json())
+      .then((data) => {
+        setPokemons(data);
+        setLoading(false);
+      })
+  };
+
+  const getPreviousResults = () => {
+    setLoading(true);
+    fetch(pokemons.previous)
+      .then((response) => response.json())
+      .then((data) => {
+        setPokemons(data);
+        setLoading(false);
+      })
+  };
+
+  const addFavoPokemon = (pokemon) => {
+    setFavoPokemons([...favoPokemons, pokemon]);
+  }
+
+  const previousDisabled = pokemons.previous == null;
+  const nextDisabled = pokemons.next == null;
+
   return (
-    <div
-      className="todo"
-      style={{textDecoration: todo.isCompleted ? "line-through" : ""}}>
-        {todo.text}
+    <div className="App">
+      <h1>Pokedex</h1>
+      <div>
+        <h2>Favoriete Pokemon</h2>
         <div>
-          <button onClick={() => completeTodo(index)}>Complete</button>
-          <button onClick={() => removeTodo(index)}>x</button>
+          <ul>
+            {favoPokemons.map(favoPokemons => {
+              return (
+                <li>
+                  {favoPokemons}
+                </li>)
+            })}
+          </ul>
         </div>
-    </div>
-  )
-}
-
-//function TodoForm
-function TodoForm({addTodo}){
-  const [value, setValue] = useState("");
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (!value) return;
-    addTodo(value);
-    setValue("");
-  }
-
-  //return statement
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        className="input"
-        value={value}
-        onChange={e => setValue(e.target.value)}
-      />
-    </form>
-  )
-}
-
-//function for adding todo's completing and removing todo's
-function App(){
-  const addTodo = text => {
-    const newTodos = [...todos, {text}];
-    setTodos(newTodos);
-  }
-
-  const completeTodo = index => {
-    const newTodos = [...todos];
-    newTodos[index].isCompleted = true;
-    setTodos(newTodos);
-  }
-
-  const removeTodo = index => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
-  }
-
-  const [todos, setTodos] = useState([
-    {
-    text: "Learn about React",
-    isCompleted: false 
-    },
-    {
-      text: "Meet friend for lunch",
-      isCompleted: false
-    },
-    {
-      text: "Build really cool todo app",
-      isCompleted: false
-    }
-  ]);
-
-  return (
-    <div className="app">
-      <div className="todo-list">
-        {todos.map((todo, index) => (
-          <Todo
-          key={index}
-          index={index}
-          todo={todo}
-          completeTodo={completeTodo}
-          removeTodo={removeTodo}
-          />
-        ))}
-        <TodoForm addTodo={addTodo}/>
+        <div>
+          <button onClick={getPreviousResults} disabled={previousDisabled}>Vorige</button>
+          <button onClick={getNextResults} disabled={nextDisabled}>Volgende</button>
+        </div>
+      </div>
+      <div className='pokemon-list'>
+        {pokemons.results.map(pokemon => {
+          return (
+            <Pokemon
+              name={pokemon.name}
+              url={pokemon.url}
+              add={addFavoPokemon}
+            />
+          )
+        })}
       </div>
     </div>
-  )
+  );
 }
+
 export default App;
---
+++ src/Pokemon.jsx
@@ -0,0 +1,48 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function Pokemon({ name, url, add }) {
+  var [pokemons, setPokemon] = useState({});
+  var [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setPokemon(data);
+        setLoading(false);
+      });
+  }, [url]);
+
+  const capitalizeLetterFirst = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  if (loading) {
+    return (
+      <div className="pokemon">
+        Loading...
+      </div>
+    )
+  }
+
+  return (
+    <div className="pokemon">
+      <div className='pokemon-details'>
+        <img src={pokemons.sprites.front_default} alt={pokemons.name} />
+        <div>
+          <h2>{capitalizeLetterFirst(pokemons.name)}</h2>
+          <div>
+            <ol>
+              <li>{pokemons.weight / 10}kg</li>
+              <li>{pokemons.height / 10}m</li>
+            </ol>
+          </div>
+        </div>
+        <button onClick={() => add(pokemons.name)}>Add to Favorites</button>
+      </div>
+    </div>
+  );
+}
+
+export default Pokemon;
